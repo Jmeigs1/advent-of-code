bb9660722d330fbe36fd408778722b583693f15b
Jacob Meigs <1722522+Jmeigs1@users.noreply.github.com>
2024-12-08 03:36:53 -0500

    Ocaml make fooling

--- 2024/1/ocaml/part1.ml
+++ 2024/1/ocaml/part1.ml
@@ -2,6 +2,8 @@ let default = "input.txt" in
 let file = if Array.length Sys.argv == 2 then Sys.argv.(1) else default in
 let ic = open_in file in
 
+let _test = Str.regexp in
+
 let s = really_input_string ic (in_channel_length ic) in
 
 let split_on_whitespace s =
