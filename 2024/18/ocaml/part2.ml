341e245432eef1b3bf8b9bc9209cfc70e9bfc513
Jacob Meigs <1722522+Jmeigs1@users.noreply.github.com>
2024-12-18 17:28:06 -0500

    Add day 18 part 2

--- 2024/18/ocaml/part2.ml
+++ 2024/18/ocaml/part2.ml
@@ -1,6 +1,120 @@
 open Core
+open Re
 
-let _split_on_whitespace s = s |> Str.split (Str.regexp "[ ]+")
+type cell = int * int
+
+let split_to_parts s = s |> Str.split (Str.regexp "\n\n")
+let get_nums = Re.matches Re.(alt [ rg '0' '9' ] |> rep1 |> compile)
+
+(* --- tpl --- *)
+let abs_diff_tpl (i, j) (h, k) = Int.abs (i - h) + Int.abs (j - k)
+let add_tpl (i, j) (h, k) = (i + h, j + k)
+let eq_tpl (i, j) (h, k) = i = h && j = k
+let mul_tpl (i, j) (h, k) = (i * h, j * k)
+
+let to_pairs lst =
+  let rec to_pairs lst out =
+    match lst with
+      | h :: h2 :: tail -> to_pairs tail ((h, h2) :: out)
+      | _ -> out
+  in
+  to_pairs lst []
+
+let size_x = 71
+let size_y = 71
+let start = (0, 0)
+let finish = (70, 70)
+let cap = 1024
+
+(* --- alt --- *)
+(* let size_x = 7 *)
+(* let size_y = 7 *)
+(* let start = (0, 0) *)
+(* let finish = (6, 6) *)
+(* let cap = 12 *)
+
+let is_in_bounds cell =
+  let cx, cy = cell in
+  let x = cx >= 0 && cx < size_x in
+  let y = cy >= 0 && cy < size_y in
+  x && y
+
+let get_adj start =
+  let dirs = [ (1, 0); (0, 1); (-1, 0); (0, -1) ] in
+  dirs |> List.map ~f:(add_tpl start)
+
+let find_min opts =
+  let min =
+    Map.fold ~init:None
+      ~f:(fun ~key ~data a ->
+        if Option.is_none a then Some (key, data)
+        else
+          let a_key, a_data = Option.value_exn a in
+          if a_data > data then Some (key, data) else Some (a_key, a_data))
+      opts
+  in
+  match min with
+    | None -> failwith "bad find_min"
+    | Some (cell, cost) -> (cell, cost)
+
+let traverse blocks =
+  let seen = Set.Poly.empty in
+  let options = Map.Poly.empty |> Map.set ~key:start ~data:0 in
+
+  let rec traverse seen options =
+    match Map.is_empty options with
+      | true -> None
+      | false ->
+          let current_cell, current_cost = find_min options in
+          if Poly.equal current_cell finish then Some current_cost
+          else
+            let n_options = Map.remove options current_cell in
+            let n_seen = Set.add seen current_cell in
+
+            (* printf "Options:\n"; *)
+            (* Map.iter_keys options ~f:(fun (i, j) -> printf "%d:%d\n" i j); *)
+            (* let adj_test = *)
+            (*   get_adj current_cell *)
+            (*   |> List.filter ~f:(fun c -> not (Set.mem blocks c)) *)
+            (* in *)
+            let adj =
+              get_adj current_cell
+              |> List.filter ~f:(fun c -> not (Set.mem blocks c))
+              |> List.filter ~f:is_in_bounds
+              |> List.filter ~f:(fun c -> not (Set.mem n_seen c))
+              |> List.map ~f:(fun x -> (x, current_cost + 1))
+            in
+
+            (* printf "Adj :\n"; *)
+            (* List.iter adj_test ~f:(fun (i, j) -> printf "%d:%d\n" i j); *)
+            let n_options =
+              List.fold_left adj ~init:n_options ~f:(fun a (cell, cost) ->
+                  if not (Map.mem a cell) then Map.set a ~key:cell ~data:cost
+                  else
+                    let old_cost = Map.find_exn a cell in
+                    match cost < old_cost with
+                      | true -> Map.set a ~key:cell ~data:cost
+                      | false -> a)
+            in
+
+            (* printf "New options:\n"; *)
+            (* Map.iter_keys n_options ~f:(fun (i, j) -> printf "%d:%d\n" i j); *)
+            (* printf "\n"; *)
+            traverse n_seen n_options
+  in
+  traverse seen options
+
+(* --- searching backwards is much faster -- *)
+(* --- can make a bin search if need more speed - lazy -- *)
+let rec loop blocks last =
+  let filtered_blocks = blocks |> Set.Poly.of_list in
+  let cost = traverse filtered_blocks in
+  match cost with
+    | Some _ -> last
+    | None ->
+    match blocks with
+      | h :: tail -> loop tail h
+      | [] -> failwith "ran out of blocks in loop"
 
 let () =
   let default = "input.txt" in
@@ -8,6 +122,9 @@ let () =
   let file = if Int.equal (Array.length args) 2 then args.(1) else default in
 
   let s = In_channel.read_all file in
-  let lines = s |> String.strip |> String.split_on_chars ~on:[ '\n' ] in
 
-  1 |> List.nth_exn lines |> print_endline
+  let blocks = get_nums s |> List.map ~f:Int.of_string |> to_pairs in
+
+  let fx, fy = loop blocks (0, 0) in
+  printf "%d,%d\n" fx fy;
+  ()
