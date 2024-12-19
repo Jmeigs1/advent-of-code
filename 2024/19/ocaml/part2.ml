53b67957b28ad2b6e8fb8a883ba1170c198b6754
Jacob Meigs <1722522+Jmeigs1@users.noreply.github.com>
2024-12-19 18:06:46 -0500

    Remove part 1 filtering from day 19 part 2

--- 2024/19/ocaml/part2.ml
+++ 2024/19/ocaml/part2.ml
@@ -10,11 +10,6 @@ let split_to_parts s =
 
 let get_nums = Re.(alt [ rg '0' '9' ] |> rep1 |> compile)
 
-let join lst sep =
-  match lst with
-    | h :: t -> List.fold_left ~init:h ~f:(fun a s -> a ^ sep ^ s) t
-    | [] -> ""
-
 let match_str ptrn str =
   if String.length ptrn > String.length str then None
   else
@@ -60,25 +55,10 @@ let () =
   let p1, p2 = split_to_parts s in
   let ptrns = p1 |> String.strip |> Str.split (Str.regexp ", ") in
 
-  let have_towels_for = join ptrns "|" in
-
-  let tst_regex =
-    Re.Perl.re ({|^(?:|} ^ have_towels_for ^ {|)+$|}) |> Re.compile
-  in
-
   let lines = p2 |> String.strip |> String.split_lines in
 
-  let test_line l =
-    Re.exec_opt tst_regex l |> function
-    | Some v -> true
-    | None -> false
-  in
-
-  let filtered_lines = List.filter lines ~f:test_line in
-
   let count =
-    filtered_lines
-    |> List.fold_left ~init:0 ~f:(fun a s -> a + count_matches ptrns s)
+    lines |> List.fold_left ~init:0 ~f:(fun a s -> a + count_matches ptrns s)
   in
 
   printf "%d\n" count;
